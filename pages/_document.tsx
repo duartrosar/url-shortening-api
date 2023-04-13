49797820ca81a032298d3b74fad63821c1bac9ea
duartrosar <74115478+duartrosar@users.noreply.github.com>
2023-04-13 10:59:25 +0100

    Fix Head title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Frontend Mentor - Shortly URL shortening API Challenge</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,16 @@ import LinkShortener from "@/components/LinkShortener";
 import Statistics from "@/components/Statistics";
 import Boost from "@/components/Boost";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Frontend Mentor - Shortly URL shortening API Challenge</title>
+      </Head>
       <main className={`${inter.className} overflow-x-hidden bg-[#EFF1F7]`}>
         <Hero />
         <LinkShortener />
